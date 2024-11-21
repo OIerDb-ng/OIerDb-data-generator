4e9edc3461b19934bf0b60ae844f7233e9068812
yhx-12243 <yhx12243@gmail.com>
2024-11-21 09:32:09 +0800

    fix(#348): 附属小学, resolve #350

--- record.py
+++ record.py
@@ -124,6 +124,7 @@ class Record:
                 # 在高中毕业后又有小学记录的情况下，不应该合并
                 if (a.contest.school_year() < b.contest.school_year() and
                     any(level in a.school.name for level in ['高中', '中学', '高级']) and
+                    '小学' not in a.school.name and
                     '小学' in b.school.name):
                     return inf
 
